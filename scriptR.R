89cecd073b6a59537cb333a55667bc814c71ddb3
rayanasouza <you@example.com>
2018-09-16 11:08:32 -0300

    -a

--- scriptR.R
+++ scriptR.R
@@ -1,4 +1,7 @@
 library(tidyverse)
+library(ggplot2)
+ggplot2::ggplot()
 
+mpg <- read.csv("C:/Users/rayan/Documents/datasciencewithR/mpg.csv")
 carros <- mpg
 ggplot(data = carros) + geom_point(mapping = aes(x = displ, y = hwy))
