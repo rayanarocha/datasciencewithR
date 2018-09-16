eb6084c01bd1c0969f92bd60ecf4d4e94e20c6bc
rayanasouza <you@example.com>
2018-09-16 17:19:26 -0300

    -a

--- scriptR.R
+++ scriptR.R
@@ -4,4 +4,17 @@ ggplot2::ggplot()
 
 mpg <- read.csv("C:/Users/rayan/Documents/datasciencewithR/mpg.csv")
 carros <- mpg
+
 ggplot(data = carros) + geom_point(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = carros) + geom_point(mapping = aes(x = displ, y = hwy, color = class))
+
+ggplot(data = carros) + geom_point(mapping = aes(x = displ, y = hwy, size = class))
+
+ggplot(data = carros) + geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
+
+ggplot(data = carros) + geom_point(mapping = aes(x = displ, y = hwy, color = "blue"))
+
+?mpg
+
+ggplot(data = carros) + geom_point(mapping = aes(x = year, y = cyl, shape = class))
