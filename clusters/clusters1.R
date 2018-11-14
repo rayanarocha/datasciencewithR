488718c72702d8e5252474260fd1e06b6731bf66
rayanasouza <you@example.com>
2018-11-14 08:35:05 -0300

    new files

--- clusters/clusters1.R
+++ clusters/clusters1.R
@@ -15,6 +15,7 @@ library(readxl)
 evadidos <- read_excel("datasciencewithR/clusters/evasao_evadidos (2).xlsx")
 View(evadidos)
 
+# descategorização dos dados
 evadidos$sexo = as.numeric(as.factor(evadidos$sexo))
 evadidos$raca = as.numeric(as.factor(evadidos$raca))
 evadidos$estado_origem = as.numeric(as.factor(evadidos$estado_origem))
@@ -27,9 +28,9 @@ evadidos$renda = as.numeric(as.factor(evadidos$renda))
 evadidos$IRA = as.numeric(as.factor(evadidos$IRA))
 evadidos$reprovacoes = as.numeric(as.factor(evadidos$reprovacoes))
 
-plot(evadidos$municipio_origem, type = "p", main = "plot(x, type = \"s\")", cex = 1, col = "blue")
-plot(evadidos$raca)
-plot(evadidos$sexo, type = "p", xlab = "a", ylab = "b")
+plot(evadidos$municipio_origem, type = "p", main = "plot(x, type = \"s\")", cex = 1, col = "blue") #plotagem dos dados da base por coluna
+plot(evadidos$raca) #plotagem dos dados da base por coluna
+plot(evadidos$sexo, type = "p", xlab = "a", ylab = "b") #plotagem dos dados da base por coluna
 
 head(evadidos)
 #data(evadidos, package = "rattle")
@@ -50,15 +51,4 @@ c1
 plot(evadidos, col = kmf$cluster)
 
 clusplot(df, kmf$cluster, main = "2D representation of Cluster", shade = TRUE, labels = 5, lines = 2, stand = TRUE)
-pdf("datasciencewithR/clusters/grafico.pdf", width = 20, height = 14)
-dev.off()
 
-#pam(evadidos, 2, diss = inherits(df, "dist"),
- #   metric = c("euclidean", "manhattan"), 
-  #  medoids = NULL, stand = FALSE, cluster.only = FALSE,
-   # do.swap = TRUE,
-    #keep.diss = FALSE && !cluster.only && n < 100,
-    #keep.data = FALSE && !cluster.only,
-    #pamonce = FALSE, trace.lev = 0)
-
-#clusplot(pam(df, 2))
